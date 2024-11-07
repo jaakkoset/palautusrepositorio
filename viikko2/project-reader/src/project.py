26950e1ac1e73b30ccd9be9792e034ff33a14b4e
Jaakko Airikkala <jaakkel@gmail.com>
2024-11-07 18:31:00 +0200

    Week 2 ex 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,35 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        license: str,
+        authors: list,
+        dependencies: list,
+        dev_dependencies: list,
+    ):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
+    def _listify_list(self, list: list):
+        return "\n- " + "\n- ".join(list) if len(list) > 0 else "-"
+
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license}"
+            f"\n"
+            f"\nAuthors {self._listify_list(self.authors)}"
+            f"\n"
+            f"\nDependencies: {self._listify_list(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies: {self._listify_list(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,67 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        # print(content)
+        # print("type:", type(content))
+
+        parsed = toml.loads(content)
+        # print(parsed)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        name = parsed["tool"]["poetry"]["name"]
+        description = parsed["tool"]["poetry"]["description"]
+        license = parsed["tool"]["poetry"]["license"]
+        authors = parsed["tool"]["poetry"]["authors"]
+        dependencies = [
+            dependency for dependency in parsed["tool"]["poetry"]["dependencies"]
+        ]
+        dev_dependencies = [
+            dependency
+            for dependency in parsed["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        ]
+        # dependencies = [
+        #     f"{dependency} = {version}"
+        #     for dependency, version in parsed["tool"]["poetry"]["dependencies"].items()
+        # ]
+        # dev_dependencies = [
+        #     f"{dependency} = {version}"
+        #     for dependency, version in parsed["tool"]["poetry"]["group"]["dev"][
+        #         "dependencies"
+        #     ].items()
+        # ]
+        return Project(
+            name, description, license, authors, dependencies, dev_dependencies
+        )
+
+
+# Sanakirja jonka toml.loads palauttaa
+{
+    "tool": {
+        "poetry": {
+            "name": "Ohtutesting app",
+            "version": "0.0.1",
+            "description": "Sovellus joka toimii testisyötteenä ohtun viikon 2 laskareihin",
+            "authors": ["Matti Luukkainen", "Kalle Ilves"],
+            "license": "MIT",
+            "dependencies": {
+                "python": "^3.10",
+                "Flask": "^3.0.0",
+                "editdistance": "^0.6.2",
+            },
+            "group": {
+                "dev": {
+                    "dependencies": {
+                        "coverage": "^7.3.2",
+                        "robotframework": "^6.1.1",
+                        "robotframework-seleniumlibrary": "^6.1.3",
+                        "requests": "^2.31.0",
+                    }
+                }
+            },
+        }
+    },
+    "build-system": {
+        "requires": ["poetry-core"],
+        "build-backend": "poetry.core.masonry.api",
+    },
+}
