e6c99b6cea531952757ab827baf3e8777a88c52c
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-01 18:53:39 +0200

    Week 5 ex 3: refactor tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,71 @@
 class TennisGame:
+    LOVE = 0
+    FIFTEEN = 1
+    THIRTY = 2
+    FORTY = 3
+
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_score = self.player1_score + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score = self.player2_score + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        if self.player1_score == self.player2_score:
+            score = self.draw()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            score = self.more_than_three_points()
+
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            score = ""
+            score += self.less_than_four_points(self.player1_score)
+            score += "-"
+            score += self.less_than_four_points(self.player2_score)
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+        return score
+
+    def draw(self):
+        if self.player1_score == self.LOVE:
+            score = "Love-All"
+        elif self.player1_score == self.FIFTEEN:
+            score = "Fifteen-All"
+        elif self.player1_score == self.THIRTY:
+            score = "Thirty-All"
+        else:
+            score = "Deuce"
+
+        return score
+
+    def more_than_three_points(self):
+        minus_result = self.player1_score - self. player2_score
+
+        if minus_result == 1:
+            score = "Advantage player1"
+        elif minus_result == -1:
+            score = "Advantage player2"
+        elif minus_result >= 2:
+            score = "Win for player1"
+        else:
+            score = "Win for player2"
+
+        return score
+
+    def less_than_four_points(self, player_score):
+        if player_score == self.LOVE:
+            score = "Love"
+        elif player_score == self.FIFTEEN:
+            score = "Fifteen"
+        elif player_score == self.THIRTY:
+            score = "Thirty"
+        else:
+            score = "Forty"
 
         return score
